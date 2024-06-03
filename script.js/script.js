70766c547c8c61745bf9ce921f31922b4841468a
NnIiKkTtAa <ivanovaanastasia905@gmail.com>
2024-06-03 16:26:08 +0500

    задание

--- script.js/script.js
+++ script.js/script.js
@@ -1,4 +1,37 @@
-for (let i=10; i <= 20; i++ ){
-    document.write(i * i + "<br>");
-}
+var div_res = document.getElementById("res");
+
+function check_username_and_password() {
+    var username = form.username.value;
+    var password = form.password.value;
+    var symbols_in_password = password.length;
+
+    div_res.innerHTML = "";
+
+    if (username == "") {
+        div_res.innerHTML = "имя не должно быть пустым";
+    }
+
+    if (password == "") {
+        div_res.innerHTML += "<br>пароль не должен быть пустым";
+    } 
     
+    if (symbols_in_password <= 6) {
+        div_res.innerHTML += "<br>пароль должен быть больше шести символов";
+    } 
+    
+    if (!hasUppercase(password)) {
+        div_res.innerHTML += "<br>пароль должен содержать одну заглавную букву";
+    }
+
+    is_number_in_string(password);
+}
+
+function hasUppercase(password) {
+    for (var i = 0; i < password.length; i++) {
+        var ch = password.charAt(i);
+        if (ch === ch.toUpperCase() && ch !== ch.toLowerCase()) {
+            return true;
+        }
+    }
+    return false;
+}
